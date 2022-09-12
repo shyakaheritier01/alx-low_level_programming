606242020b10fcf2234eb4cbfb645ef420ce2bd0
shyakaheritier01 <shyakaheritier01@gmail>
2022-09-12 03:11:13 -0700

    positive or negative

---
+++ 4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+* main -print the string in the put function
+*
+*description: using the main function
+*this program prints "programing is like building a multilingual puzzle
+*   
+
