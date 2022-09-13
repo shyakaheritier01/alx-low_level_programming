aac1a08b4f0139e94e4e0f163f20dbd49a9ce66d
shyakaheritier01 <shyakaheritier01@gmail>
2022-09-13 00:38:12 -0700

    positive or negative

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#!include <stdio.h>
+
+/**
+*min - print the string in the put function
+*
+* Description: 0
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf(size of an int:% ld byte(s)\n", sizeof(i));
+	printf("size of an int:% ld byte(s)\n", sizeof(i));
