375b1aa4b72736e5e03eeafb8340f169fe89cb8d
shyakaheritier01 <shyakaheritier01@gmail>
2022-09-18 08:25:15 -0700

    putchar printed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,37 +1,32 @@
+#!/bin/bash
 #include "main.h"
-
-
 
 /**
 
- * print_alphabet - print alphabet of letters
+ * print_alphabet - function to print abc
 
  *
 
- * Return: void
+ * Return: 0
 
  */
-
-
 
 void print_alphabet(void)
 
 {
 
-	int i;
+	char c;
 
 
 
-	for (i = 'a'; i <= 'z'; i++)
-
-
+	for (c = 'a'; c <= 'z'; c++)
 
 	{
 
-		_putchar(i);
+		_putchar(c);
 
 	}
 
-	_putchar('\n');
+_putchar('\n');
 
 }
