fd9e61b2c1634ca5373404cc7722075636076df9
shyakaheritier01 <shyakaheritier01@gmail>
2022-09-26 07:20:26 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,93 +1,24 @@
 #include "main.h"
-
-
-
 /**
 
- * print_times_table - prints the n times table, starting with 0
+ * _islower - a function that checks for lowercase character
 
- * @n: number of the times table
+ * @c: single letter input
+
+ * Return: 1 if int c is lowercase, 0 if otherwise
 
  */
 
-void print_times_table(int n)
+int _islower(int c)
 
 {
 
-	int i, j, k;
+	if (c >= 'a' && c <= 'z')
 
+		return (1);
 
+	else
 
-	if (n >= 0 && n <= 15)
-
-	{
-
-		for (i = 0; i <= n; i++)
-
-		{
-
-			for (j = 0; j <= n; j++)
-
-			{
-
-				k = j * i;
-
-				if (j == 0)
-
-				{
-
-					_putchar(k + '0');
-
-				} else if (k < 10 && j != 0)
-
-				{
-
-					_putchar(',');
-
-					_putchar(' ');
-
-					_putchar(' ');
-
-					_putchar(' ');
-
-					_putchar(k + '0');
-
-				} else if (k >= 10 && k < 100)
-
-				{
-
-					_putchar(',');
-
-					_putchar(' ');
-
-					_putchar(' ');
-
-					_putchar((k / 10) + '0');
-
-					_putchar((k % 10) + '0');
-
-				} else if (k >= 100)
-
-				{
-
-					_putchar(',');
-
-					_putchar(' ');
-
-					_putchar((k / 100) + '0');
-
-					_putchar(((k / 10) % 10) + '0');
-
-					_putchar((k % 10) + '0');
-
-				}
-
-			}
-
-			_putchar('\n');
-
-		}
-
-	}
+		return (0);
 
 }
