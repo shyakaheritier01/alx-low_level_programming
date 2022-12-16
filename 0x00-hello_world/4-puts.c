fd77c141ec9394cbfa7278c8cb3ac738133172f6
shyakaheritier01 <shyakaheritier01@gmail.com>
2022-12-16 02:14:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,43 +1,19 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 /**
  *
- * * main - Printing
+ *  * main - main block
  *
- * *
+ *   * Return: 0
  *
- * * Return: Always 0 (Successful)
- *
- * */
+ *    */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual 
-
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
-
-vim 5-printf.c
-
-#include <stdio.h>
-
-/**
- *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-int main(void)
-
-{
-
-	printf("with proper grammar, but the outcome is a piece o
