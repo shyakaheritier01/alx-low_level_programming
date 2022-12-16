27f4374cb0b5f4a6b8047130814bc6aad2f0b45d
shyakaheritier01 <shyakaheritier01@gmail.com>
2022-12-16 02:00:59 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
 
- * main - main block
-
- * Return: 0
-
- */
-
-=======
-nclude <stdio.h>
-/**
- * main - main block
- * Return: 0
- */
->>>>>>> 5b47cf4a32a363919c0745247c99ad114da8dafa
 int main(void)
 
 {
-<<<<<<< HEAD
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
-
-=======
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
->>>>>>> 5b47cf4a32a363919c0745247c99ad114da8dafa
+
 }
