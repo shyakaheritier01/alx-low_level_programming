9ee1f928d1d988882800cb8a71948424906d01e2
shyakaheritier01 <shyakaheritier01@gmail.com>
2022-12-16 05:39:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 nclude <stdio.h>
-
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
