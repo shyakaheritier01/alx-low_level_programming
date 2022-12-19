38a3115a0d30d872424ca6d9580e3329f568bb2f
root <shyakaheritier01@gmail.com>
2022-12-19 08:21:00 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
-int _atoi(char *s);
+int _atoi(char *s)
+
 #endif
