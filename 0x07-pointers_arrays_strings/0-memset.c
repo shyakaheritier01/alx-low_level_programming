bbe76331ca64b9cd327846c1e37b49d3a7b5a336
shyakaheritier01 <shyakaheritier01@gmail.com>
2023-01-03 04:04:39 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,18 @@
-#include "main.h"                                                                                                                    
-
-                                                                                                                                     
+#include "main.h"
 
 /**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+{
+	unsigned int i;
 
- * _memset - Fills the first n bytes of the memory area                                                                              
-
- *          pointed to by @s with the constant byte @c.                                                                              
-
- * @s: A pointer to the memory area to be filled.                                                                                    
-
- * @c: The character to fill the memory area with.                                                                                   
-
- * @n: The number of bytes to be filled.                                                                                             
-
- * description _memset: over there                                                                                                   
-
- *                                                                                                                                   
-
- * Return: A pointer to the filled memory area @s.                                                                                   
-
- *                                                                                                                                   
-
- */                                                                                                                                  
-
-void *_memset(void *s, int c, size_t n)                                                                                              
-
-{                                                                                                                                    
-
-        unsigned int index;                                                                                                          
-
-        unsigned char *memory = s, value = c;                                                                                        
-
-                                                                                                                                     
-
-        for (index = 0; index < n; index++)                                                                                          
-
-                memory[index] = value;                                                                                               
-
-                                                                                                                                     
-
-        return (memory);                                                                                                             
-
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
-
