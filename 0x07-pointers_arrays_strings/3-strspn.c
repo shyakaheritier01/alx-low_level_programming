f5c2c615285d16e9e118fe540b484b341ef318cb
shyakaheritier01 <shyakaheritier01@gmail.com>
2023-01-04 03:28:13 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strspn - Entry point
  * @s: input
@@ -14,8 +13,15 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
+			 if (*s == accept[r])
+			 {
+				 n++;
+				 break;
+			 }
+			 else if (accept[r + 1] == '\0')
+				 return (n);
+		}
+		s++;
+	}
+	return (n);
+}
