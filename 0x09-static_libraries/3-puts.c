0d5cc52678d0e56f8da7c02bba3644cb7c9664df
shyakaheritier01 <shyakaheritier01@gmail.com>
2023-01-11 09:06:35 -0800

    My task

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,7 +2,7 @@
 void _puts(char *str)
 #include<stdio.h>
 	/**
-	 * main - Entry poin
+	 * main - Entry point
 	 *
 	 * Return: Always 0 (Success)
 	 */
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -16,9 +16,8 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
-			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (0)
+	return (0);
 }
